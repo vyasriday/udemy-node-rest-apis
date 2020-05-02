6d30413a551e0050892040e889b66d8fd7a22b38
Hridayesh Sharma <dev.hridayesh@gmail.com>
2020-05-02 23:45:44 +0530

    added basic express app

---
+++ express-app/app.js
@@ -0,0 +1,78 @@
+require('dotenv').config();
+const express = require('express');
+const Joi = require('@hapi/joi');
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 4000;
+const courses = [
+  {
+    id: 1,
+    name: 'Node Complete Course',
+    price: '$10',
+  },
+  {
+    id: 2,
+    name: 'React Complete Guide',
+    price: '$12',
+  },
+  {
+    id: 3,
+    name: 'Angular 8: From Beginner to End',
+    price: '$15',
+  },
+  {
+    id: 4,
+    name: 'Machnine Learning: Zero to Mastery',
+    price: '$15',
+  },
+];
+
+app.get('/', (req, res) => {
+  res.send('Hello World!!!');
+});
+
+app.get('/api/courses', (req, res) => {
+  res.send(courses);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+  // req.params stores route params as string.
+  const id = parseInt(req.params.id);
+  const course = courses.find((course) => course.id === id);
+  if (!course) {
+    res.status(404).send('Resource not found');
+    return;
+  }
+  res.status(200).send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+  if (!req.body.name) {
+    res.status(400).send('data is not formatted properly');
+    return;
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+    price: req.body.price,
+  };
+  res.status(200).send(course);
+});
+
+app.put('/api/courses/:id', (req, res) => {
+  const id = parseInt(req.id);
+  const course = courses.find((course) => course.id === id);
+  if (!course) {
+    res.status(400).send('Course does not exist!');
+    return;
+  }
+  courses = courses.map((course) => {
+    if (course.id === id) {
+      return { ...course };
+    }
+    return course;
+  });
+  return courses;
+});
+
+app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
